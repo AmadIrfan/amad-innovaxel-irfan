e16823d77c7cb7c235511aad58d02c09a2b3b83f
amadirfan <amadirfan443@gmail.com>
2025-04-20 18:20:42 +0500

    update short URL route

--- backend/controllers/urlController.js
+++ backend/controllers/urlController.js
@@ -42,6 +42,31 @@ class UrlController {
         }
     }
 
+    static async updateUrls(req, res) {
+        try {
+
+            const { shortCode } = req.params;
+            const { url } = req.body;
+            const entry = await Url.findOne({ shortCode });
+
+            if (!entry) {
+                return res.status(404).json('Short Code not Found');
+            }
+            if (!url) {
+                return res.status(400).json('New URL is required');
+            }
+            entry.url = url;
+            entry.updatedAt = new Date();
+            await entry.save();
+            const response = entry.toObject();
+            // @ts-ignore
+            delete response.accessCount;
+            return res.status(200).json(response);
+        } catch (err) {
+            return res.send(err.message);
+        }
+    }
+
 
 
 }
